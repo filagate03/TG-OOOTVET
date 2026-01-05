f89e79460ba4b0b95e76cdb2383b7a05130ff464
filagate03 <alexfarley555@gmail.com>
2026-01-05 19:00:51 +0300

    add frontend/vite.config.js

---
+++ frontend/vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  base: './',
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8002',
+        changeOrigin: true
+      },
+      '/media': {
+        target: 'http://localhost:8002',
+        changeOrigin: true
+      }
+    }
+  }
+})
